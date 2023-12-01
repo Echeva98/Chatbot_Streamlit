c15006c2ed342476731a9a33edb2d378754ecc97
Alejandro Echevarria Roig <alexecheva@hotmail.com>
2023-12-01 13:29:40 +0100

    4th

--- app_like_chatgpt.py
+++ app_like_chatgpt.py
@@ -37,6 +37,9 @@ temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, va
 top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
 max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
 st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -65,11 +68,27 @@ def generate_llama2_response(prompt_input):
     return output
 
 def generate_diff_response(prompt_input):
-    output = api.run(
-        "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
-        input={"prompt": prompt_input})
-        
-    return str(output).replace('[','').replace(']','').replace("'","")
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    
+    output = replicate.run('stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                "width": 768,
+                                "height": 768,
+                                "prompt": "an astronaut riding a horse on mars, hd, dramatic lighting",
+                                "scheduler": "K_EULER",
+                                "num_outputs": 1,
+                                "guidance_scale": 7.5,
+                                "num_inference_steps": 50
+                            })
+    output2 = str(output).replace('[','').replace(']','').replace("'","")
+    response = requests.get(output2)
+    image = Image.open(BytesIO(response.content))
+    return st.image(image)
 
 
 # User-provided prompt
@@ -91,3 +110,6 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
+
+
+    
